72576a7a618bd3bdf9ba3c2ecf16713a9b18a891
Rakin <rakchy1@gmail.com>
2023-11-16 01:42:17 +0000

    Uploads card details to local database. Creates separate functions for extraction, cleaning, and uploading data.

--- main.py
+++ main.py
@@ -2,27 +2,37 @@ from database_utils import DatabaseConnector
 from data_extraction import DataExtractor
 from data_cleaning import DataCleaning
 
-def write_user_data():
+def extract_user_data():
     rds_connector = DatabaseConnector('db_creds.yaml')
-    local_db_connector = DatabaseConnector('sales_data_creds.yaml')
     extractor = DataExtractor()
-    data = DataCleaning()
-
-    print(rds_connector.list_db_tables())
     table = extractor.read_rds_table(rds_connector, 'legacy_users')
-    cleaned_data = data.clean_user_data(table)
+    return table
 
+def user_data_cleaning():
+    cleaner = DataCleaning()
+    user_data = extract_user_data()
+    cleaned_user_data = cleaner.clean_user_data(user_data)
+    return cleaned_user_data
+
+def write_user_data():
+    local_db_connector = DatabaseConnector('sales_data_creds.yaml')
+    cleaned_data = user_data_cleaning()
     local_db_connector.upload_to_db(cleaned_data, 'dim_users')
 
-def extract_pdf():
+def extract_from_pdf():
     extractor = DataExtractor()
     pdf_table = extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
     return pdf_table
 
 def clean_pdf_data():
     cleaner = DataCleaning()
-    pdf_table = extract_pdf()
+    pdf_table = extract_from_pdf()
     cleaned_pdf_table = cleaner.clean_card_data(pdf_table)
     return cleaned_pdf_table
 
-print(clean_pdf_data())
+def write_card_data():
+    local_db_connector = DatabaseConnector('sales_data_creds.yaml')
+    cleaned_card_data = clean_pdf_data()
+    local_db_connector.upload_to_db(cleaned_card_data, 'dim_card_details')
+    
+write_card_data()
